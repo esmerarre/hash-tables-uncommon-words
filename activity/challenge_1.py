e3556b166d63264110b82ca01b18c8b85516c198
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-10-27 11:38:01 -0700

    activity complete

--- activity/challenge_1.py
+++ activity/challenge_1.py
@@ -1,2 +1,3 @@
 def uncommon_from_sentences(sentences):
+    sentence_dict = {}
     pass
--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,31 @@
 def uncommon_from_sentences(s1, s2):
-    pass
+    list_s1 = s1.split(" ")
+    list_s2 = s2.split(" ")
+    list_s1.extend(list_s2)
+    list_total = list_s1 #the tests are passing now. list_s1 now has the extended list, for some reason was not registering in list total
+    total_dict = dict_helper(list_total) 
+    uncommon_list = []
+    for word, count in total_dict.items():
+        if count == 1:
+            uncommon_list.append(word)
+    return uncommon_list        
+
+  
+# all the tests passed? 
+#ok
+#thank you so much for trying that 
+#all tests in test_original.py
+#good job! nice working with you guys
+#youre welcome! thanks for your help too!
+
+
+def dict_helper(list_total):
+    dict_words = {}
+    for word in list_total:
+        if word in dict_words.keys():
+            dict_words[word] += 1
+        else: 
+            dict_words[word] = 1
+
+
+    return dict_words
